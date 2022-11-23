6b934b3b1265c83b4d3d7ace02d69a93f9dcefd0
parvezahmedweb1 <parvezahmedweb1@gmail.com>
2022-11-23 23:43:29 +0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        lg: "3rem",
+      },
+    },
+    extend: {
+      colors: {
+        primary: "#E3FFE6",
+        secondary: "#0F172A",
+      },
+    },
+  },
+  plugins: [],
+};
